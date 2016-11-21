e4bfab043bb2b1cef227f4f88533e2eaba11e70c
Toshiba <Toshiba@Toshiba-PC>
2016-11-21 18:29:04 +0100

    aaa

--- HotelManagementSystem/src/ba/ibu/edu/oop/Hotel.java
+++ HotelManagementSystem/src/ba/ibu/edu/oop/Hotel.java
@@ -1,6 +1,11 @@
 package ba.ibu.edu.oop;
 
+import javax.swing.JOptionPane;
+
 public class Hotel {
 	
-	
+	public static void main(String[] args) {
+		
+		JOptionPane.showMessageDialog(null, "Hello Hotel");
+	}
 }
