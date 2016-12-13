75c22bee73d7f8958e643df352d45e319ffb4481
mujoh <m.hadzimehanovic@gmail.com>
2016-12-13 18:13:09 +0100

    aaa

--- HotelManagementSystem/src/ba/ibu/edu/oop/GymManagementWindow.java
+++ HotelManagementSystem/src/ba/ibu/edu/oop/GymManagementWindow.java
@@ -66,7 +66,7 @@ public class GymManagementWindow {
 				
 				LoginEmployee loginEmp = new LoginEmployee();
 				loginEmp.loginEmployee();
-				JOptionPane.showMessageDialog(null, "Hi");
+				JOptionPane.showMessageDialog(null, "Hii");
 			}
 		});
 		Empbutton.setBounds(59, 200, 104, 41);
